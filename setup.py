f37d7512ac779ee4cd47beb59af3b342ed1b7140
amoamirmansouri <mansouri23c@gmail.com>
2024-07-22 05:17:29 -0700

    2

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name="fixer",
-      version="0.1",
+      version="0.2",
       license="MIT",
       packages=["fixer"],
       url="git@github.com:amoamirmansouri/lsn2.git",
-      requires=["requests"]
+      requires=["numpy"]
       )
